4d0e3c3b771aeb2461dc4995b1346b0fc8963540
anantsarin <anant.sarin@gmail.com>
2016-10-24 23:10:09 -0400

    clean commit

--- myproject/myproject/create.sql
+++
@@ -1,6 +0,0 @@
-BEGIN;
---
--- Create model Post
---
-CREATE TABLE `blog_post` (`id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY, `title` varchar(140) NOT NULL, `body` longtext NOT NULL, `date` datetime(6) NOT NULL);
-COMMIT;
